aa484f83ed57b021de6ec461234f93754b2ad794
Carlos Gonzalez Obando <cagonzalezob@est.utn.ac.cr>
2024-11-23 21:38:36 -0600

    Cimentacion del proyecto

---
+++ Model/Produccion/FinishCrops.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Produccion;
+
+/**
+ *
+ * @author carlo
+ */
+public class FinishCrops {
+    private double produccionPorcentage;
+}
